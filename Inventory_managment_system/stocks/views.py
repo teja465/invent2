91e5570f490748f032034717a02265fca12c8172
teja465 <katukuri.raviteja465@gmail.com>
2020-06-02 17:55:25 +0530

    first commit

---
+++ Inventory_managment_system/stocks/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from stocks.models import *
+from stocks.forms import *
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def check(r):
+    return render(r,'check.html')
+
+def index_view(r):
+    return render(r,'index.html')
+def display_mobiles(r):
+    #
+    mobiles=mobile.objects.all()
+    return render(r,'mobiles.html',{'mobiles':mobiles})
+
+
+@login_required
+def  delete_mob(request,id):
+    print('in delete_mob view')
+    mob=mobile.objects.get(id=id)
+    mob.delete()
+    return HttpResponseRedirect('/')
+
+
+@login_required
+def update_mob(request,id):
+
+    form=mobile.objects.get(id=id)
+    if request.method=='POST':
+        #
+        form=mobile_form(request.POST,instance=form)
+        form.save()
+        return HttpResponseRedirect('/')
+
+
+    return render(request,'update_mob.html',{'form':form})
